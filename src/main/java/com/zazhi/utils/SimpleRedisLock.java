5ec63e7bdfe92c3d6e583a09e7ebbacfe9fc8829
zazhi <2065707071@qq.com>
2025-02-16 19:25:23 +0800

    fix: 解决分布式问题1

--- src/main/java/com/zazhi/utils/SimpleRedisLock.java
+++ src/main/java/com/zazhi/utils/SimpleRedisLock.java
@@ -1,5 +1,6 @@
 package com.zazhi.utils;
 
+import cn.hutool.core.lang.UUID;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.util.concurrent.TimeUnit;
@@ -20,18 +21,30 @@ public class SimpleRedisLock implements ILock {
     }
 
     private static final String KEY_PREFIX="lock:";
+    private static final String ID_PREFIX= UUID.randomUUID().toString(true) + "-";
+
 
     @Override
     public boolean lock(long timeoutSec) {
-        String threadName = Thread.currentThread().getName();
-        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadName, timeoutSec, TimeUnit.SECONDS);
+        // 获取线程标识
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+
+        Boolean success = stringRedisTemplate.opsForValue().setIfAbsent(KEY_PREFIX + name, threadId, timeoutSec, TimeUnit.SECONDS);
         // 直接返回 success 有可能会拆箱时空指针异常
         return Boolean.TRUE.equals(success);
     }
 
     @Override
     public boolean unlock() {
-        Boolean success = stringRedisTemplate.delete(KEY_PREFIX + name);
-        return Boolean.TRUE.equals(success);
+        // 获取线程标示
+        String threadId = ID_PREFIX + Thread.currentThread().getId();
+        // 获取锁中的标示
+        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+
+        if(threadId.equals(id)){
+            Boolean success = stringRedisTemplate.delete(KEY_PREFIX + name);
+            return Boolean.TRUE.equals(success);
+        }
+        return false;
     }
 }
